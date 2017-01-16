33dcbba6edfc194cf367388ce811777561179713
timocodex <timothy.og.siahaan@gmail.com>
2017-01-16 15:02:06 +0700

    all release done

--- setup.js
+++ setup.js
@@ -1,3 +1,41 @@
 "use strict"
 
-//write your code here
+const repl = require("repl");
+const sqlite = require('sqlite3').verbose()
+
+var file = 'student.db'
+var db = new sqlite.Database(file)
+
+//SQL Statement
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT,birthdate DATE)"
+var SEED_DATA = "INSERT INTO student (firstname,lastname,birthdate) VALUES ('timo','codex','1993-06-28');"
+
+//CREATE_TABLE
+let createTable = () =>{
+  db.serialize(function(){
+    db.run(CREATE_TABLE,function(err){
+      if(err){
+        console.log(err);
+      } else{
+        console.log('CREATE TABLE');
+      }
+    })
+  })
+}
+
+let seedData = () =>{
+  db.serialize(function(){
+    db.run(SEED_DATA,function(err){
+      if(err){
+        console.log(err);
+      } else{
+        console.log('SEED DATA');
+      }
+    })
+  })
+}
+
+var rep = repl.start('> ').context
+
+rep.createTable = createTable
+rep.seedData = seedData
