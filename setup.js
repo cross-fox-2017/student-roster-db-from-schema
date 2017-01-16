753c2c1ac99065ca33a473283d42129094d60825
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-16 15:56:45 +0700

    dgana done all release

--- setup.js
+++ setup.js
@@ -1,3 +1,39 @@
 "use strict"
 
-//write your code here
+const repl = require('repl');
+const sqlite3 = require('sqlite3').verbose();
+
+let file = "student.db"
+let db = new sqlite3.Database(file);
+
+class Setup {
+  static createTable () {
+    db.serialize(function() {
+      let CREATE_TABLE = "CREATE TABLE student(id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE)"
+      db.run(CREATE_TABLE, function(err) {
+        if(err) {
+          console.log(err)
+        } else {
+          console.log("Create table Success!")
+        }
+      })
+    })
+  }
+
+  static seedData (firstname, lastname, birthdate) {
+    let SEED_DATA = `INSERT INTO student(firstname, lastname, birthdate) VALUES ('${firstname}', '${lastname}', '${birthdate}')`
+    db.serialize(function() {
+      db.run(SEED_DATA, function(err) {
+        if(err) {
+          console.log(err)
+        } else {
+          console.log("Insert Data Success!")
+        }
+      })
+    })
+  }
+}
+let command = repl.start("> ")
+
+command.context.createTable = Setup.createTable;
+command.context.insertData = Setup.seedData;
